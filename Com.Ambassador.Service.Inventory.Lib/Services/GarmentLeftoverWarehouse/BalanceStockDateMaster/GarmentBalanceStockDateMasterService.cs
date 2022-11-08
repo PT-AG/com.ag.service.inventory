f86631f0bb6d8708af3e4a57544d8484e0fa7738
Lucia Widyana <mo_cia@yahoo.com>
2022-11-08 10:33:56 +0700

    addModulGarmentBalanceStockDateMaster

---
+++ Com.Ambassador.Service.Inventory.Lib/Services/GarmentLeftoverWarehouse/BalanceStockDateMaster/GarmentBalanceStockDateMasterService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse.BalanceStockDateMaster
+{
+    class GarmentBalanceStockDateMasterService
+    {
+    }
+}
