69ac71b14595d509622b750e84ea3f2662f7b391
Lucia Widyana <mo_cia@yahoo.com>
2022-11-09 14:53:03 +0700

    add mater stock Opname date

--- Com.Ambassador.Service.Inventory.Lib/Services/GarmentLeftoverWarehouse/BalanceStockDateMaster/GarmentBalanceStockDateMasterService.cs
+++ Com.Ambassador.Service.Inventory.Lib/Services/GarmentLeftoverWarehouse/BalanceStockDateMaster/GarmentBalanceStockDateMasterService.cs
@@ -22,7 +22,7 @@ namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse
         private readonly IServiceProvider ServiceProvider;
         private readonly IIdentityService IdentityService;
 
-
+       
         public GarmentBalanceStockDateMasterService(InventoryDbContext dbContext, IServiceProvider serviceProvider)
         {
             DbContext = dbContext;
@@ -31,7 +31,7 @@ namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse
             ServiceProvider = serviceProvider;
             IdentityService = (IIdentityService)serviceProvider.GetService(typeof(IIdentityService));
 
-        }
+     }
 
         public async Task<int> CreateAsync(GarmentBalanceStockDateMaster model)
         {
@@ -44,7 +44,7 @@ namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse
                     model.FlagForCreate(IdentityService.Username, UserAgent);
                     model.FlagForUpdate(IdentityService.Username, UserAgent);
 
-
+                   
                     DbSet.Add(model);
                     Created = await DbContext.SaveChangesAsync();
 
@@ -70,7 +70,7 @@ namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse
             GarmentBalanceStockDateMaster model = new GarmentBalanceStockDateMaster();
             PropertyCopier<GarmentBalanceStockDateMasterViewModel, GarmentBalanceStockDateMaster>.Copy(viewModel, model);
 
-
+        
             return model;
         }
 
@@ -79,7 +79,7 @@ namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse
             GarmentBalanceStockDateMasterViewModel viewModel = new GarmentBalanceStockDateMasterViewModel();
             PropertyCopier<GarmentBalanceStockDateMaster, GarmentBalanceStockDateMasterViewModel>.Copy(model, viewModel);
 
-
+          
             return viewModel;
         }
 
@@ -130,6 +130,5 @@ namespace Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse
         {
             throw new NotImplementedException();
         }
-
     }
 }
