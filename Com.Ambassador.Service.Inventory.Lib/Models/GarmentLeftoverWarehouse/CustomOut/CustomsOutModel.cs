3a3167a7c22f60985cfc82cd140a41772436c0e7
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-02-05 14:55:49 +0700

    Enhance-AddModul-AvalCustomsOut

---
+++ Com.Ambassador.Service.Inventory.Lib/Models/GarmentLeftoverWarehouse/CustomOut/CustomsOutModel.cs
@@ -0,0 +1,26 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Com.Ambassador.Service.Inventory.Lib.Models.GarmentLeftoverWarehouse.CustomOut
+{
+    public class CustomsOutModel : StandardEntity,IValidatableObject
+    {
+        [MaxLength(255)]
+        public string BCNo { get; set; }
+        public DateTimeOffset BCDate { get; set; }
+        [MaxLength(20)]
+        public string BCType { get; set; }
+        public bool IsSubcon { get; set; }
+        [MaxLength(3000)]
+        public string Remark { get; set; }
+        public virtual ICollection<CustomsOutItemModel> Items { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Com.Ambassador.Service.Inventory.WebApi/Controllers/v1/GarmentLeftoverWarehouse/CustomOut/CustomOutController.cs
@@ -0,0 +1,25 @@
+﻿using Com.Ambassador.Service.Inventory.Lib.Models.GarmentLeftoverWarehouse.CustomOut;
+using Com.Ambassador.Service.Inventory.WebApi.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.Ambassador.Service.Inventory.Lib.ViewModels.GarmentLeftoverWarehouse.CustomOut;
+using Com.Ambassador.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse.CustomsOuts;
+using Com.Ambassador.Service.Inventory.Lib.Services;
+
+namespace Com.Ambassador.Service.Inventory.WebApi.Controllers.v1.GarmentLeftoverWarehouse.CustomOut
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/garment/leftover-customs-out")]
+    [Authorize]
+    public class CustomOutController : BaseController<CustomsOutModel, CustomOutVM, ICustomsOutService>
+    {
+        public CustomOutController(IIdentityService identityService, IValidateService validateService, ICustomsOutService service) : base(identityService, validateService, service, "1.0.0")
+        {
+        }
+    }
+}
